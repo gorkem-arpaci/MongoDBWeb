2469ad1ebbf58af200d9a034e2ce278451b8dd79
Görkem <grrarr.1907@gmail.com>
2024-12-05 14:48:35 +0300

    connected

---
+++ app_api/models/db.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var dbURI = "mongodb+srv://gorkemarpaci19:aX7BmQkdxTEoUIuE@gorko.7nd3b.mongodb.net/"
+
+mongoose.connect(dbURI)
+mongoose.connection.on("connected", function(){
+    console.log("Mongoose connected to " + dbURI)
+})
+
+mongoose.connection.on("disconnected", function(err){
+    console.log("kapandı")
+})
+
+process.on("SIGINT", function(){
+    mongoose.connection.close()
+    console.log("MongoDB bağlantısı kesildi, uygulama durduruldu.")
+    process.exit(0)
+})
